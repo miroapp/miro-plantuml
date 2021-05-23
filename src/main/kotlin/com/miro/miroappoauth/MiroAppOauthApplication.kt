d891cd744cd0201f9c27a705b6c8fa776bd98708
seregamorph <serega.morph@gmail.com>
2021-05-23 11:20:03 +0300

    spring initializr

---
+++ src/main/kotlin/com/miro/miroappoauth/MiroAppOauthApplication.kt
@@ -0,0 +1,11 @@
+package com.miro.miroappoauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiroAppOauthApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiroAppOauthApplication>(*args)
+}
---
+++ src/test/kotlin/com/miro/miroappoauth/MiroAppOauthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.miro.miroappoauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiroAppOauthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
