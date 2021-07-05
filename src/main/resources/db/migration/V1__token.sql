b4b69b0ba4dc3a5d2521cfcd3860e148e06d46e6
seregamorph <serega.morph@gmail.com>
2021-07-05 17:45:44 +0200

    payload size fix

--- src/main/resources/db/migration/V1__token.sql
+++ src/main/resources/db/migration/V1__token.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE token (
     access_token VARCHAR(64) PRIMARY KEY,
-    access_token_payload VARCHAR(256) NOT NULL,
+    access_token_payload VARCHAR(512) NOT NULL,
     user_id BIGINT NOT NULL,
     team_id BIGINT NOT NULL,
     state VARCHAR(64) NOT NULL,
