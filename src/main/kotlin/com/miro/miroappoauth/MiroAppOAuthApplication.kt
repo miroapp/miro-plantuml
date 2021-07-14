1f504f0b38a4f8fc8887f80f6e182216852b9c09
seregamorph <serega.morph@gmail.com>
2021-07-14 12:47:41 +0200

    h2-console log address

--- src/main/kotlin/com/miro/miroappoauth/MiroAppOAuthApplication.kt
+++ src/main/kotlin/com/miro/miroappoauth/MiroAppOAuthApplication.kt
@@ -2,7 +2,9 @@ package com.miro.miroappoauth
 
 import com.miro.miroappoauth.config.AppProperties
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties
 import org.springframework.boot.autoconfigure.web.ServerProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
@@ -11,9 +13,13 @@ import org.springframework.context.event.EventListener
 
 @EnableConfigurationProperties(AppProperties::class)
 @SpringBootApplication
-class MiroAppOAuthApplication(
-    private val serverProperties: ServerProperties
-) {
+class MiroAppOAuthApplication {
+
+    @Autowired
+    private lateinit var serverProperties: ServerProperties
+
+    @Autowired
+    private lateinit var h2ConsoleProperties: H2ConsoleProperties
 
     private val log = LoggerFactory.getLogger(MiroAppOAuthApplication::class.java)
 
@@ -24,6 +30,14 @@ class MiroAppOAuthApplication(
             log.info("Management server started at {}://localhost:{}/manage", protocol, event.webServer.port)
         } else {
             log.info("Server started at {}://localhost:{}", protocol, event.webServer.port)
+            if (h2ConsoleProperties.enabled) {
+                log.info(
+                    "H2 console started at {}://localhost:{}{} ({})",
+                    protocol, event.webServer.port, h2ConsoleProperties.path,
+                    if (h2ConsoleProperties.settings.isWebAllowOthers)
+                        "!!!accessible remotely, e.g. via tunnel proxy!!!" else "not accessible remotely"
+                )
+            }
         }
     }
 }
