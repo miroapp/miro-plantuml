8b78b3c69b579b215a4bf142f2e0cfc88457306b
Sergey Chernov <sergey.ch@miro.com>
2022-02-25 12:15:23 +0100

    appName query parameter

--- src/main/webapp-sdk1/src/index.ts
+++ src/main/webapp-sdk1/src/index.ts
@@ -1,15 +1,17 @@
 import icon from './icon.svg'
 
 miro.onReady((): void => {
+  let appName = new URLSearchParams(window.location.search).get("appName");
+  let title = appName == null ? 'miro-app-oauth' : appName;
   miro.initialize({
     extensionPoints: {
       toolbar: {
-        title: 'miro-app-oauth',
+        title: title,
         toolbarSvgIcon: icon,
         librarySvgIcon: icon,
         async onClick(): Promise<void> {
           // Remember that 'app.html' resolves relative to index.js file. So app.html have to be in the /dist/ folder.
-          await miro.board.ui.openLibrary('/webapp-sdk1/app.html', {title: 'miro-app-oauth'})
+          await miro.board.ui.openLibrary('/webapp-sdk1/app.html', {title: title})
         },
       },
     },
