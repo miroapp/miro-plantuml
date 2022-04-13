adcb9a52025ea1c549ab0305f611b227f3c683cb
seregamorph <serega.morph@gmail.com>
2022-04-13 11:30:05 +0200

    import sdk2

--- src/main/webapp-sdk2/src/app.tsx
+++ src/main/webapp-sdk2/src/app.tsx
@@ -1,52 +1,84 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import cn from 'classnames'
+import axios, {AxiosResponse} from "axios";
 
 function App() {
-  const [isRendering, setRendering] = React.useState(false);
+    // const [isRendering, setRendering] = React.useState(false);
 
-  async function addSomeElements() {
-    setRendering(true);
+    // async function addSomeElements() {
+    //     setRendering(true);
+    //
+    //     const shape1 = await miro.board.createShape({
+    //         content: 'Hello, World!',
+    //         shape: 'cloud'
+    //     });
+    //
+    //     await miro.board.createShape({
+    //         content: 'See ya',
+    //         shape: 'hexagon',
+    //         x: shape1.x + 200,
+    //         y: shape1.y
+    //     });
+    //
+    //     setRendering(false);
+    //     await miro.board.ui.closeModal();
+    // }
 
-    const shape1 = await miro.board.createShape({
-      content: 'Hello, World!',
-      shape: 'cloud'
-    });
+    async function getSelfUser() {
+        const backendUrl = new URL(window.location.href)
+        backendUrl.pathname = "/get-self-user"
 
-    await miro.board.createShape({
-      content: 'See ya',
-      shape: 'hexagon',
-      x: shape1.x + 200,
-      y: shape1.y
-    });
+        const token = await miro.board.getIdToken()
+        //resetAuthState()
+        axios.get(backendUrl.href,
+            {
+                headers: {
+                    "X-Miro-Token": token
+                }
+            })
+            .then((response: AxiosResponse) => {
+                console.error(`callBackend: "${response.data.name}"`)
+                //miro.v1.showNotification(`callBackend: user name="${response.data.name}"`)
+            })
+            .catch((error) => {
+                let message = error.message
+                if (error.response) {
+                    message = JSON.stringify(error.response.data)
+                } else if (error.request) {
+                    message = "request: " + error.request
+                }
 
-    setRendering(false);
-    await miro.board.ui.closeModal();
-  }
+                console.error(`callBackend error: "${message}"`)
+                alert(`callBackend: error "${message}"`)
+            });
+    }
 
-  return (
-    <div className="grid wrapper">
-      <h1 className="h1">Mermaid</h1>
-      <div className="cs1 ce12">
-        <a className="link link-primary" href="https://github.com/mermaid-js/mermaid" target="_blank">Documenation</a>
-      </div>
-      <div className="cs1 ce12 form-group">
-          <textarea className="textarea" placeholder="Code" rows={10} spellCheck={false}></textarea>
-      </div>
-      <div className="cs1 ce12">
-        <button
-          className={cn('button', 'button-primary', {
-            'button-loading': isRendering
-          })}
-          onClick={addSomeElements}
-          type="button"
-          disabled={isRendering}
-        >
-          {isRendering ? '' : 'Render'}
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="grid wrapper">
+            <h1 className="h1">Mermaid</h1>
+            <div className="cs1 ce12">
+                <a className="link link-primary" href="https://github.com/mermaid-js/mermaid"
+                   target="_blank">Documenation</a>
+            </div>
+            <div className="cs1 ce12 form-group">
+                <textarea className="textarea" placeholder="Code" rows={10} spellCheck={false}></textarea>
+            </div>
+            <div className="cs1 ce12">
+                <button
+                    className={cn('button', 'button-primary', {
+                        'button-loading': isRendering
+                    })}
+                    // onClick={addSomeElements}
+                    onClick={getSelfUser}
+                    type="button"
+                    disabled={isRendering}
+                >
+                    {isRendering ? '' : 'Render'}
+                </button>
+            </div>
+        </div>
+    );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
